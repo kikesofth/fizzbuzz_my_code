b8e78f8055f88b78980354b6e6242d8248c50c54
kikesofth <kikesofth@gmail.com>
2025-09-01 20:26:58 -0600

    Se añadieron comentarios de referencia al codigo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 numero = int(input('Ingresa el numero de interacciones a comprobar impares: '))
 
 def fizzbuzz(numero):
+    # Haciendo un recorrido qie se dio en el anterior paso
     for i in range(1, numero +1):
+        #Comparando si al dividir el numero dado por 5 o 0 da 0
         if i % 3 == 0 and i % 5 == 0:
             print('FizzBuzz')
         elif i % 3 == 0:
@@ -11,5 +13,5 @@ def fizzbuzz(numero):
         else:
             print(i)
 
-
+# Se llama a la funcion para que imprima
 fizzbuzz(numero)
